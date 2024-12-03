68a877b7d1c5c81cb3a16af80c4abcde2e8d852d
yagnik0 <yashyagnik456@.com>
2024-12-03 14:43:37 +0530

    third commit

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileMenu.classList.toggle('active')
   })
 });
+
+// Scrolling navbar 
+
+window.addEventListener('scroll', function(){
+  const navbar = document.querySelector('.navbar')
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar--scroll')
+  } else {
+    navbar.classList.remove('navbar--scroll')
+  }
+})
